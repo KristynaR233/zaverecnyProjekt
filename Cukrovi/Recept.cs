701bd87dac3afec9f0b00356061887d60feff02f
Kristyna Randova <randova.kristyna@seznam.cz>
2025-06-12 15:57:29 +0200

    vytvoreni tridy recept a zasoby

---
+++ Cukrovi/Recept.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Cukrovi;
+
+public class Recept
+{
+    public string Nazev { get; set; }
+    public string Suroviny { get; set; }
+    public string Postup { get; set; }
+
+
+}
---
+++ Cukrovi/Zasoby.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cukrovi;
+
+public class Zasoby
+{
+    public string Surovina { get; set; }
+    public double Mnozstvi { get; set; }
+    public string Jednotky{ get; set; }
+
+}
