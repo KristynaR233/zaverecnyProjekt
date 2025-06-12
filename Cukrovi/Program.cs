9b6ee26e4cbb238b12711ab7b165d38f0fd69c22
Kristyna Randova <randova.kristyna@seznam.cz>
2025-06-12 15:40:58 +0200

    zacatek projektu

---
+++ Cukrovi/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+
+namespace Cukrovi;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string pomlcky = new string('-', 20);
+        Console.WriteLine(pomlcky + "ZAVERECNY PROJEKT" + pomlcky);
+        Console.WriteLine("Uzivateli, zdej sve jmeno:");
+        string jmeno = Console.ReadLine();
+        DateTime stedryDen = new DateTime(2025, 12, 24);
+        TimeSpan pocetDniDoVanoc = stedryDen - DateTime.Now;
+        Console.WriteLine($"Ahoj {jmeno}, do Vanoc zbyva {pocetDniDoVanoc.Days} dni.");
+        Console.WriteLine("Vyber akci:");
+        Console.WriteLine("1-zobrazit recepty");
+        Console.WriteLine("2-zobrazit zasoby");
+        string vstupUzivatele = Console.ReadLine();
+        switch (vstupUzivatele)
+        {
+            case "1":
+                {
+                    Console.WriteLine("Vyber cislo receptu, ktery chces zobrazit:");
+                    Console.WriteLine("1- vanilkove rohlicky");
+                    Console.WriteLine("2- linecke");
+                    Console.WriteLine("3- pracny");
+                    Console.WriteLine("4- muslicky s orechovou naplni");
+                    string vyberReceptu = Console.ReadLine();
+                    switch (vyberReceptu)
+                    {
+                        case "1":
+                            {
+                                break;
+                            }
+                        case "2":
+                            {
+                                break;
+                            }
+                        case "3":
+                            {
+                                break;
+                            }
+                        case "4":
+                            {
+                                break;
+                        }
+                        
+                        
+                    }
+                    break;
+                }
+            case "2":
+                {
+                    break;
+                    }
+
+        }
+
+
+
+    }
+}
