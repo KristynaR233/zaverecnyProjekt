1570355c91fe2b3c33a4c933a6b0f2d55f531bb6
Kristyna Randova <randova.kristyna@seznam.cz>
2025-06-13 21:52:36 +0200

    pridani tridy pracny

--- Cukrovi/Recept.cs
+++ Cukrovi/Recept.cs
@@ -7,6 +7,18 @@ public class Recept
     public string Nazev { get; set; }
     public string Suroviny { get; set; }
     public string Postup { get; set; }
+    
+    public void VypisRecept()
+    {
+        Console.WriteLine($"{Nazev}");
+        Console.WriteLine($"Seznam suroviny: {Suroviny.Split(",")}");
+        Console.WriteLine($"Postup: {Postup}");
+       
+
+    }
+
+
+}
 
 
 }
