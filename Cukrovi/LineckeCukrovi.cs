993757c025f821bc359e710e1bd976e8f23b4cb2
Kristyna Randova <randova.kristyna@seznam.cz>
2025-06-13 21:31:48 +0200

    pridani tridy linecke cukrovi

---
+++ Cukrovi/LineckeCukrovi.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Cukrovi;
+
+public class LineckeCukrovi
+{   string nazev = "Lincke cukrovi";
+    string suroviny = "140 g masla, 70 g cukr moucka, 210 g hladka mouka, 2 vejce, marmelada";
+
+    string postup = "Mouku prosijte na val, pridejte cukr, maslo, zloutky. Co nejrychleji zpracovat testo a dat do lednicky. Troubu predehrat na 180stupnu. Z testa vyvalet plat silny cca 2mm. Vykrajovat kolecka, do poloviny vykrojit prazdny stred a nezme je prenaset na plech. Pect cca 5 minut. Vychladle cukrovi slepit marmeladou.";
+
+    public void VypisRecept()
+    {
+        Console.WriteLine($"{nazev}");
+        Console.WriteLine($"Seznam suroviny: {suroviny.Split(",")}");
+        Console.WriteLine($"Postup: {postup}");
+       
+
+    }
+
+
+}
