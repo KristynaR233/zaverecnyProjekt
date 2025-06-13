a84c6556f141f6f92be8e924d8bd53f7b347e4d1
Kristyna Randova <randova.kristyna@seznam.cz>
2025-06-13 23:47:42 +0200

    pridani tridy muslicky

---
+++ Cukrovi/Muslicky.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Cukrovi;
+
+public class Muslicky : Recept
+{   
+    
+    string nazev = "Muslicky s orechovou naplni";
+    string suroviny = "250 g masla, 250 g cukr moucka, 250 g hladka mouka, 2 vejce, 100 g mletych orechu,  ";
+    string postup = "Smichat mouku, maslo, 1 vejce a 1 lzici vody a vypracuj hladke testo. Nech odpocinout v lednici. Na napln smichat mlete orechy, 100g mouckoveho cukru a 1 vejce. Predehrat troubu na 180 stupnu. Z testa vyvalet plat cca 2 mm silny a vykrajet kolecka. Na kazde kolecko dat trochu naplne a prelozit napul. Muslicky prenest na plech a pect cca 10 minut do svetle zlate barvy. Jeste horke obalit v mouckovem cukru.";
+
+
+
+
+}
+
+
