d0fae23e9ba72b48ea783fe04c591e9f702f8db1
Kristyna Randova <randova.kristyna@seznam.cz>
2025-06-14 14:25:28 +0200

    odebrani samostatnych class curkovi

--- Cukrovi/Program.cs
+++ Cukrovi/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Cukrovi;
 
@@ -13,6 +14,36 @@ class Program
         DateTime stedryDen = new DateTime(2025, 12, 24);
         TimeSpan pocetDniDoVanoc = stedryDen - DateTime.Now;
         Console.WriteLine($"Ahoj {jmeno}, do Vanoc zbyva {pocetDniDoVanoc.Days} dni. Je cas zacit pect cukrovi.");
+
+ 
+
+     Recept lineckeCukrovi = new Recept
+     ( "Linecke cukrovi",
+       "140 g masla, 70 g cukr moucka, 210 g hladka mouka, 2 vejce, marmelada",
+       "Mouku prosijte na val, pridejte cukr, maslo, zloutky. Co nejrychleji zpracovat testo a dat do lednicky. Troubu predehrat na 180stupnu. Z testa vyvalet plat silny cca 2mm. Vykrajovat kolecka, do poloviny vykrojit prazdny stred a nezme je prenaset na plech. Pect cca 5 minut. Vychladle cukrovi slepit marmeladou."
+
+     );
+
+        Recept muslicky = new Recept
+        (
+            "Muslicky s orechovou naplni",
+            "250 g masla, 250 g cukr moucka, 250 g hladka mouka, 2 vejce, 100 g mletych orechu,  ", 
+            "Smichat mouku, maslo, 1 vejce a 1 lzici vody a vypracuj hladke testo. Nech odpocinout v lednici. Na napln smichat mlete orechy, 100g mouckoveho cukru a 1 vejce. Predehrat troubu na 180 stupnu. Z testa vyvalet plat cca 2 mm silny a vykrajet kolecka. Na kazde kolecko dat trochu naplne a prelozit napul. Muslicky prenest na plech a pect cca 10 minut do svetle zlate barvy. Jeste horke obalit v mouckovem cukru."
+
+        );
+        Recept pracny = new Recept
+        ( "Pracny",
+            "225 g masla, 70 g cukr moucka, 225 g hladka mouka, 50 g mletych orechu, 2 hrebicky, 0.5 lzicky skorice, 0.5 lzice kakaa",
+            "Prisady zpracovat na pevne testo a polovinu obarvit kakaem. Nechat odpocinout v lednicce.Troubu predehrat na 170 stupnu.  Potom vytlacit do formicek. Pest zhruba 5-7 minut. Po upeceni vyklepnout z formicek."
+
+        );
+        Recept vanilkoveRohlicky = new Recept
+        (   "Vanilkove rohlicky",
+            "170 g masla, 50 g cukr moucka, 210 g hladka mouka, 80 g vlaskych orechu, 20 g vanilkovy cukr ",
+            "Vychlazene maslo nakrajet na kosticky, pridat ostatni suroviny a vypracovat pevne testo. Testo nechat odpocivat v lednicce. Pote z testa vyvylet silny valecek, nakrajet ho na kousky a ty potom tvarovat jako rohlicky. Troubu predehrat na 180 stupnu. Rohlicky pect 12-15 minut. Po upeceni nechat kratce zchladnout a pote je obalit ve smesi mouckoveho a vanilkoveho cukru."
+
+        );
+       
 
         Console.WriteLine("Vyber akci:");
         Console.WriteLine("1-zobrazit recepty");
@@ -32,19 +63,22 @@ class Program
                     {
                         case "1":
                             {
-                                VanilkoveRohlicky.VypisRecept();
+                                vanilkoveRohlicky.VypisRecept();
                                 break;
                             }
                         case "2":
                             {
+                                lineckeCukrovi.VypisRecept();
                                 break;
                             }
                         case "3":
                             {
+                                pracny.VypisRecept();
                                 break;
                             }
                         case "4":
                             {
+                                muslicky.VypisRecept();
                                 break;
                         }
                         
@@ -55,7 +89,7 @@ class Program
             case "2":
                 {
                     Console.WriteLine("Mate doma tyto suroviny:");
-                    //SkladZasob.VypisZasoby();
+                    
                     break;
                     }
 
--- Cukrovi/Recept.cs
+++ Cukrovi/Recept.cs
@@ -7,13 +7,20 @@ public class Recept
     public string Nazev { get; set; }
     public string Suroviny { get; set; }
     public string Postup { get; set; }
+
+    public Recept(string nazev, string suroviny, string postup)
+    {
+        Nazev = nazev;
+        Suroviny = suroviny;
+        Postup = postup;
+    }
     
     public void VypisRecept()
     {
         Console.WriteLine($"{Nazev}");
         Console.WriteLine($"Seznam suroviny: {Suroviny.Split(",")}");
         Console.WriteLine($"Postup: {Postup}");
-       
+
 
     }
 
@@ -21,4 +28,4 @@ public class Recept
 }
 
 
-}
+
